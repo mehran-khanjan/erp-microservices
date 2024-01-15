8d6d2b28ad11c30aa25829567b0471cf4da2b591
unknown <mehrankh1913@gmail.com>
2024-01-15 15:45:00 +0330

    add config folder

---
+++ src/main/java/com/relicblock/enterprise/configs/MyDataRestConfig.java
@@ -0,0 +1,27 @@
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                                                     CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {
+                HttpMethod.POST,
+                HttpMethod.PATCH,
+                HttpMethod.DELETE,
+                HttpMethod.PUT};
+
+        config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(Review.class);
+        config.exposeIdsFor(Message.class);
+
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+        disableHttpMethods(Review.class, config, theUnsupportedActions);
+        disableHttpMethods(Message.class, config, theUnsupportedActions);
+
+        /* Configure CORS Mapping */
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+}
