ed570801e84f25cbabb11d7a78ceed15292bd389
unknown <mehrankh1913@gmail.com>
2024-01-25 09:38:06 +0330

    add more codes

--- src/main/java/com/relicblock/enterprise/configs/SecurityConfiguration.java
+++ src/main/java/com/relicblock/enterprise/configs/SecurityConfiguration.java
@@ -7,5 +7,16 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Disable Cross Site Request Forgery
         http.csrf().disable();
+
+        // Protect endpoints at /api/<type>/secure
+        http.authorizeRequests(configurer ->
+                        configurer
+                                .antMatchers("/api/books/secure/**",
+                                        "/api/reviews/secure/**",
+                                        "/api/messages/secure/**",
+                                        "/api/admin/secure/**")
+                                .authenticated())
+                .oauth2ResourceServer()
+                .jwt();
     }
 }
