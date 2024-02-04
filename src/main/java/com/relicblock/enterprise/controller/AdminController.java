c2712ec97a7f46af907748b0186070cb88d779c1
unknown <mehrankh1913@gmail.com>
2024-02-04 10:13:19 +0330

    add more codes

---
+++ src/main/java/com/relicblock/enterprise/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.relicblock.enterprise.controller;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+}
