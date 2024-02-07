2a763786023e70479b254abacdd7d08144f99a43
unknown <mehrankh1913@gmail.com>
2024-02-07 16:31:38 +0330

    add more codes

--- src/main/java/com/relicblock/enterprise/controller/AdminController.java
+++ src/main/java/com/relicblock/enterprise/controller/AdminController.java
@@ -11,4 +11,14 @@ public class AdminController {
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
     }
+
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value="Authorization") String token,
+                                     @RequestParam Long bookId) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only");
+        }
+        adminService.increaseBookQuantity(bookId);
+    }
 }
