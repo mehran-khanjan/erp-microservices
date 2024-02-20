ce2db1946f9052534ec8108ec703ce0f3a197c12
unknown <mehrankh1913@gmail.com>
2024-02-20 09:13:17 +0330

    add more codes

---
+++ src/main/java/com/relicblock/enterprise/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.relicblock.enterprise.controller;
+
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+}
