680a9f68b6e79d1d529d1613d76fc73eab99fc2e
unknown <mehrankh1913@gmail.com>
2024-03-11 10:53:34 +0400

    add more codes

---
+++ src/main/java/com/relicblock/enterprise/controller/MessagesController.java
@@ -0,0 +1,13 @@
+package com.relicblock.enterprise.controller;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+    private MessagesService messagesService;
+
+    @Autowired
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+}
