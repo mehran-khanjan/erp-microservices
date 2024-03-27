04ccc31012b79e2b46fa1c2397593a1704f06e79
unknown <mehrankh1913@gmail.com>
2024-03-27 18:59:59 +0330

    add more codes

--- src/main/java/com/relicblock/enterprise/controller/ReviewController.java
+++ src/main/java/com/relicblock/enterprise/controller/ReviewController.java
@@ -10,4 +10,15 @@ public class ReviewController {
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
     }
+
+    @GetMapping("/secure/user/book")
+    public Boolean reviewBookByUser(@RequestHeader(value="Authorization") String token,
+                                    @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if (userEmail == null) {
+            throw new Exception("User email is missing");
+        }
+        return reviewService.userReviewListed(userEmail, bookId);
+    }
 }
