2f3a58e1fc996a8f3eb3dbb9b8b7db031f3c8391
Mehran Khanjan <mehrankh1913@gmail.com>
2024-05-06 09:56:26 +0400

    add more codes

--- src/main/java/com/relicblock/enterprise/entity/Book.java
+++ src/main/java/com/relicblock/enterprise/entity/Book.java
@@ -7,4 +7,13 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
 }
