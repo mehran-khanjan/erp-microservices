b52c930f3d26d0c44f5f6c18c97bc638c1cf5f88
Mehran Khanjan <mehrankh1913@gmail.com>
2024-06-17 10:29:40 +0400

    add more codes

---
+++ src/main/java/com/relicblock/enterprise/entity/History.java
@@ -0,0 +1,18 @@
+@Entity
+@Table(name = "History")
+@Data
+public class History {
+
+    public History(){}
+
+    public History(String userEmail, String checkoutDate, String returnedDate, String title,
+                   String author, String description, String img) {
+        this.userEmail = userEmail;
+        this.checkoutDate = checkoutDate;
+        this.returnedDate = returnedDate;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.img = img;
+    }
+}
